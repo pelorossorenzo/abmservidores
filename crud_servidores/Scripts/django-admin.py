fc5e39c17a82de5707bddf0637528fc9a1bf77e6
pelorossorenzo <pelorossorenzo@gmail.com>
2020-03-12 01:32:15 -0300

    Mi primer commit

---
+++ crud_servidores/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\renzo\documents\programacion\proyectos\crud_servidores\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
