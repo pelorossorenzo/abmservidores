e12aa461cb27a139b0c7057fa58aac48c837a700
pelorossorenzo <pelorossorenzo@gmail.com>
2020-03-12 02:00:42 -0300

    Mi segundo commit

--- crud_servidores/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\renzo\documents\programacion\proyectos\crud_servidores\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
